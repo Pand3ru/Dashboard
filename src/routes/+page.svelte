4557e28fe3d1e70cf883dfa341ca86849feed25a
Your Name <57118272+Pand3ru@users.noreply.github.com>
2024-08-03 10:59:43 +0200

    Weather: played aroud with animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,28 @@
-<script>
+<script lang="ts">
     import Nested from "./Weather.svelte";
     import { onMount } from "svelte";
     let width_elem;
+    let isShrinking = false;
     onMount(() => {
         width_elem = document
             .getElementById("nested-container")
             ?.getBoundingClientRect().width;
     });
+
+    function onMouseEnter() {
+        isShrinking = true;
+    }
+    function onMouseLeave() {
+        isShrinking = false;
+    }
 </script>
 
-<div id="nested-container">
+<div
+    id="nested-container"
+    class={isShrinking ? "shrink" : ""}
+    on:mouseenter={onMouseEnter}
+    on:mouseleave={onMouseLeave}
+>
     <Nested />
     <Nested />
     <Nested />
@@ -28,5 +41,16 @@
         align-items: center;
         margin: 15%;
         transform: scale(0.8);
+    }
+    .shrink {
+        animation: shrinkToRight 0.5s forwards;
+    }
+    @keyframes shrinkToRight {
+        from {
+        }
+        to {
+            transform: scaleX(0);
+            opacity: 0;
+        }
     }
 </style>
